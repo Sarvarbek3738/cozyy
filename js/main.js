cb05c2b90c6fb7999b7be18308386c02d5b2c551
Sarvarbek <sarvarbekodilxojayev898@gmail.com>
2025-01-07 19:21:41 +0500

    start

--- js/main.js
+++ js/main.js
@@ -1,14 +1,33 @@
-const minus = document.querySelector(".minus")
-const pilus = document.querySelector(".pilus")
-const sonelement = document.querySelector(".son")
-let son = 0;
-pilus.addEventListener("click", ()=>{
-    son ++;
+// const minus = document.querySelector(".minus")
+// const pilus = document.querySelector(".pilus")
+// const sonelement = document.querySelector(".son")
+// let son = 0;
+// pilus.addEventListener("click", ()=>{
+//     son ++;
+//     sonelement.textContent = son;
+// })
+// minus.addEventListener("click", ()=>{
+//     if(son>0){
+//         son --;
+//         sonelement.textContent = son;
+//     }
+// })
+
+const minus = document.querySelector(".minus");
+const pilus = document.querySelector(".pilus");
+const sonelement = document.querySelector(".son");
+let son = localStorage.getItem("son") ? parseInt(localStorage.getItem("son")) : 0;
+sonelement.textContent = son;
+pilus.addEventListener("click", () => {
+    son++;
     sonelement.textContent = son;
-})
-minus.addEventListener("click", ()=>{
-    if(son>0){
-        son --;
+    localStorage.setItem("son", son);
+});
+
+minus.addEventListener("click", () => {
+    if (son > 1) {
+        son--;
         sonelement.textContent = son;
+        localStorage.setItem("son", son);
     }
-})
+});
